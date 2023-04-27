06edb604aeea3d211670ddabb9b3d879bfe71fac
Hanna Horobchenko <h.horobchenko@soft-industry.com>
2023-04-27 11:49:54 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Anna Gorobchenko on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
